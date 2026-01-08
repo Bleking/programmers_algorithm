f43743708db777061290fcbfab240fd28452ccba
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-01-08 22:57:56 +0900

    Create 다리를_지나는_트럭.py
    프로그래머스 스택/큐 level2 다리를 지나는 트럭

---
+++ level2/다리를_지나는_트럭.py
@@ -0,0 +1,25 @@
+# 프로그래머스 스택/큐 level 2 다리를 지나는 트럭 
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+
+    currentWeight = 0
+    while len(truck_weights) != 0:
+        answer += 1
+
+        currentWeight -= bridge.popleft()
+        if currentWeight + truck_weights[0] <= weight:
+            currentWeight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+
+        else:
+            bridge.append(0)
+
+    answer += bridge_length
+
+    return answer
